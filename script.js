45a259b8599aee18b69e463ea81c3462c34d3b59
frezonline <55662236+frezonline@users.noreply.github.com>
2021-08-17 18:26:59 +0300

    v2

---
+++ script.js
@@ -0,0 +1,98 @@
+const selectors = {
+  clickAddBtn: ".addBtn",
+  myUL: ".myUL",
+  deleteAllBtn: ".deleteBtn",
+  listChecked: "ul",
+  enterInput: ".myInput",
+  inputValue: "input",
+};
+
+const clickAddBtn = document.querySelector(selectors.clickAddBtn);
+const myUL = document.querySelector(selectors.myUL);
+const deleteAllBtn = document.querySelector(selectors.deleteAllBtn);
+const listChecked = document.querySelector(selectors.listChecked);
+const enterInput = document.querySelector(selectors.enterInput);
+
+const htmlOfNote = (todoItem) => `
+  <li 
+      id=${todoItem.id} 
+      class='${todoItem.checked ? "checked" : ""}'
+      >
+      ${todoItem.text} 
+      <span class='close'>×</span> 
+  </li>`;
+
+const renderAfterReload = () => {
+  const todoStorage = JSON.parse(localStorage.getItem("TODO storage")) || [];
+  if (todoStorage.length > 0) {
+    todoStorage.forEach((item) => {
+      myUL.insertAdjacentHTML("beforeEnd", htmlOfNote(item));
+    });
+  }
+};
+
+const setNewTodo = (text) => {
+  const storage = JSON.parse(localStorage.getItem("TODO storage")) || [];
+  const newTodo = {
+    text,
+    checked: false,
+    id: Math.floor(Math.random() * 1000),
+  };
+  storage.push(newTodo);
+  localStorage.setItem("TODO storage", JSON.stringify(storage));
+  myUL.insertAdjacentHTML("beforeEnd", htmlOfNote(newTodo));
+};
+
+renderAfterReload();
+
+clickAddBtn.addEventListener("click", () => {
+  const textOfNewToDo = document.querySelector("input").value;
+  if (textOfNewToDo === "" || textOfNewToDo === " " ) {
+    alert("Please, input your task!");
+    return;
+  }
+  setNewTodo(textOfNewToDo);
+  document.querySelector("input").value = "";
+});
+
+enterInput.onkeypress = function click(event) {
+  if (event.keyCode === 13 || event.key === 13) {
+    const textOfNewToDo = document.querySelector(selectors.inputValue).value;
+    if (textOfNewToDo === "" || textOfNewToDo === " " ) {
+      alert("Please, input your task!");
+      return;
+    }
+    setNewTodo(textOfNewToDo);
+    document.querySelector("input").value = "";
+  }
+};
+
+listChecked.addEventListener("click", ({ target }) => {
+  const todoStorage = JSON.parse(localStorage.getItem("TODO storage"));
+
+  if (target.tagName === "LI") {
+    const idOfNote = +target.getAttribute("id");
+    const updatedStorage = todoStorage.map((item) => {
+      if (item.id === idOfNote) {
+        item.checked = !item.checked;
+      }
+      return item;
+    });
+    localStorage.setItem("TODO storage", JSON.stringify(updatedStorage));
+    event.target.classList.toggle("checked");
+  }
+
+  if (target.closest(".close")) {
+    const idOfNote = +target.closest("li").getAttribute("id");
+    const updatedStorage = todoStorage.filter((item) => item.id !== idOfNote);
+    localStorage.setItem("TODO storage", JSON.stringify(updatedStorage));
+    target.closest("li").remove();
+  }
+});
+
+deleteAllBtn.addEventListener("click", (event) => {
+  if (event.target.classList.contains("deleteBtn")) {
+    myUL.innerHTML = "";
+    localStorage.clear();
+  }
+});
